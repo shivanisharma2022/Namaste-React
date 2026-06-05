14daf195c5450af18daacfc6d23c45730c311676
shivanisharma2022 <sharmashivani0619@gmail.com>
2026-06-06 01:12:06 +0530

    jsx, react components, react elements

--- App.js
+++ App.js
@@ -1,44 +1,23 @@
-/**
- * <div id: "parent">
- *        <div id: "child" >
- *           <h1> I am h1 tag </h1>
- *           <h2> I am h2 tag </h2>
- *        </div>
- *       <div id: "child2" >
- *           <h1> I am h1 tag </h1>
- *           <h2> I am h2 tag </h2>
- *       </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const parent = React.createElement(
-//     "div",
-//     { id: "parent" },[
-//         React.createElement(
-//         "div",
-//         { id: "child" },[
-//         React.createElement("h1", {}, "I am h1 tag"),
-//         React.createElement("h2", {}, "I am h2 tag")
-//         ]),
-//     React.createElement(
-//         "div",
-//         { id: "child2" },[
-//         React.createElement("h1", {}, "I am h1 tag"),
-//         React.createElement("h2", {}, "I am h2 tag")
-//         ])
-//     ]);
+// Title Component
+const TitleComponent = () => ( 
+<h1 className="head" tabIndex="5">
+    Namaste React 🙏 Using JSX 
+    </h1>
+    );
 
-//     console.log(parent);
-
-    const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" },
-    "Hello World from React!");
-
-    console.log(heading); //object
-
+// React Functional Component
+const HeadingComponent = () => (
+    <div id="container">
+        {TitleComponent()}
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        <h1 className="heading" >Namaste React 🙏 Using Functional Component </h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//const root = ReactDOM.createRoot(document.getElementById("header"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
--- Readme.md
+++ Readme.md
@@ -1,3 +1,21 @@
 # Namaste React 🙏 
 
-
+# Parcel - 
+- Dev Build
+- Local Server - Hosts app to server
+- HMR = Hot Module Replacement(Automatically refreshing the page)
+- Uses File Watching Algorithm written in C++(Keeps an eye on code, as soon as something is changed, it'll build once again)
+- Caching helps in Faster Builds(inside parcel-cache) 
+- Image Optimization - costly thing to load an image on the page
+- Minification of files(At time of Production)
+- Bundling of file
+- Compression of files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling(support older browsers, generate nomodule for olders browers)
+- Diagnostic(Showing errors beautifully on browsers)
+- Error Handling
+- Host apps on HTTPs Server
+- Tree Shaking(Removes unused code for you)
+- Different bundles for development and Production
+- Developement Experience Smooth
